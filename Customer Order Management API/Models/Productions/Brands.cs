997b164259e5ec9e7574f359454b857635ce6820
Dipen Shrestha <dipenchrestha111@gmail.com>
2025-04-23 11:52:22 +0545

    edited the brand model to send custom names to client or frontend

--- Customer Order Management API/Models/Productions/Brands.cs
+++ Customer Order Management API/Models/Productions/Brands.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace Customer_Order_Management_API.Models.Productions
 {
@@ -7,10 +8,14 @@ namespace Customer_Order_Management_API.Models.Productions
     public class Brands
     {
         [Key]
+        //need using System.Text.Json.Serialization; 
+        // below annotation is used to send custom names to the client
+        [JsonPropertyName("Id")]
         public int? Brand_Id { get; set; }
 
         [Required]
         [StringLength(255)]
+        [JsonPropertyName("Name")]
         public string Brand_Name { get; set; }
     }
 }
